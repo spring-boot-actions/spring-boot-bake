3b4ed9b8e67026fd8b47ea4a6a59a6ca2e2d211a
Socheat Sok <socheatsok78@gmail.com>
2023-04-11 14:05:40 +0700

    Move implementation to spring-boot-bake.sh

---
+++ spring-boot-bake.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# shellcheck disable=SC2086
+
+SPRING_BOOT_BAKE_PATH="${GITHUB_ACTION_PATH}"
+SPRING_BOOT_BAKE_BASE_IMAGE=$1
+
+echo "Baking the Spring Boot Docker Image"
+
+# Set environment variables
+echo "SPRING_BOOT_BAKE_PATH=${SPRING_BOOT_BAKE_PATH}" >> $GITHUB_ENV
+echo "SPRING_BOOT_BAKE_BASE_IMAGE=${SPRING_BOOT_BAKE_BASE_IMAGE}" >> $GITHUB_ENV
+
+# Set outputs
+echo "bake-file=${SPRING_BOOT_BAKE_PATH}/spring-boot-bake.hcl" >> $GITHUB_OUTPUT
+echo "dockerfile=${SPRING_BOOT_BAKE_PATH}/Dockerfile" >> $GITHUB_OUTPUT
