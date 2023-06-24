899b2e3fd27b607b7e94ee6217de2cca6ac390df
Socheat Sok <socheatsok78@gmail.com>
2023-06-24 21:19:09 +0700

    Fix spring-boot-bake.sh

--- spring-boot-bake.sh
+++ spring-boot-bake.sh
@@ -11,18 +11,21 @@ if [[ -z "${GRADLE_METADATA_ACTION}" ]]; then
 fi
 
 echo "Baking the Spring Boot Docker Image"
+echo
 
 # Prepare working directory
 SPRING_BOOT_BAKE_WORKDIR="${GITHUB_WORKSPACE}/.spring-boot-bake"
 
 echo "Preparing the working directory: ${SPRING_BOOT_BAKE_WORKDIR}"
 mkdir -p "${SPRING_BOOT_BAKE_WORKDIR}"
+echo
 
 # Copying files from the action to the working directory
 echo "Copying files from the action to the working directory"
 set -x
-cp "${SPRING_BOOT_BAKE_PATH}/run.sh" "${SPRING_BOOT_BAKE_WORKDIR}"
+cp "${SPRING_BOOT_BAKE_PATH}/run.sh" "${SPRING_BOOT_BAKE_WORKDIR}/"
 set +x
+echo
 
 # Set environment variables
 echo "SPRING_BOOT_BAKE=true" >> $GITHUB_ENV
