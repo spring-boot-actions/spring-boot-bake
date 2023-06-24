959c0e30541d8c8b90b29c82c0244d4f35f1425e
Socheat Sok <socheatsok78@gmail.com>
2023-06-24 21:43:16 +0700

    Fix run.sh output

--- run.sh
+++ run.sh
@@ -10,12 +10,12 @@ CLASSPATH="${CLASSPATH:-org.springframework.boot.loader.JarLauncher}"
 
 echo "Starting Spring Boot application..."
 echo
-echo "[Main class]: $CLASSPATH"
+echo "[Main class] $CLASSPATH"
 echo
 echo "[Environment variables]"
-echo "  GLOBAL_JVM_OPTS=$GLOBAL_JVM_OPTS"
-echo "  JVM_OPTS_DEFAULT=$JVM_OPTS_DEFAULT"
-echo "  JVM_OPTS=$JVM_OPTS"
+test -n "$GLOBAL_JVM_OPTS"  && echo "  GLOBAL_JVM_OPTS=$GLOBAL_JVM_OPTS"
+test -n "$JVM_OPTS_DEFAULT" && echo "  JVM_OPTS_DEFAULT=$JVM_OPTS_DEFAULT"
+test -n "$JVM_OPTS"         && echo "  JVM_OPTS=$JVM_OPTS"
 echo
 
 exec java ${COMBINED_JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom "${CLASSPATH}"
