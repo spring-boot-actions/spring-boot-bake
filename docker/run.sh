466bd23f367d899730148451047d22bfd6f95401
Socheat Sok <alex@socheat.net>
2023-06-25 14:35:11 +0700

    Renamed SPRING_JAR_LAUNCHER -> SPRING_BOOT_JAR_LAUNCHER

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SPRING_JAR_LAUNCHER="${SPRING_JAR_LAUNCHER:-org.springframework.boot.loader.JarLauncher}"
+SPRING_BOOT_JAR_LAUNCHER="${SPRING_BOOT_JAR_LAUNCHER:-org.springframework.boot.loader.JarLauncher}"
 
 GLOBAL_JVM_OPTS="${GLOBAL_JVM_OPTS}"
 JVM_OPTS_DEFAULT="${JVM_OPTS_DEFAULT}"
@@ -9,7 +9,7 @@ COMBINED_JAVA_OPTS="${GLOBAL_JVM_OPTS} ${JVM_OPTS_DEFAULT} ${JVM_OPTS}"
 
 echo "Starting Spring Boot application..."
 echo
-echo "[Main class] $SPRING_JAR_LAUNCHER"
+echo "[Main class] $SPRING_BOOT_JAR_LAUNCHER"
 echo
 echo "[Environment variables]"
 test -n "$GLOBAL_JVM_OPTS"  && echo "  GLOBAL_JVM_OPTS=$GLOBAL_JVM_OPTS"
@@ -17,4 +17,4 @@ test -n "$JVM_OPTS_DEFAULT" && echo "  JVM_OPTS_DEFAULT=$JVM_OPTS_DEFAULT"
 test -n "$JVM_OPTS"         && echo "  JVM_OPTS=$JVM_OPTS"
 echo
 
-exec java ${COMBINED_JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom "${SPRING_JAR_LAUNCHER}"
+exec java ${COMBINED_JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom "${SPRING_BOOT_JAR_LAUNCHER}"
