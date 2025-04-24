3e9bbc3abb40c490b12ca17c842963af4b8a3613
Socheat Sok <socheatsok78@gmail.com>
2025-04-24 15:13:47 +0700

    Update java-entrypoint.sh

--- docker/java-entrypoint.sh
+++ docker/java-entrypoint.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 set -e
-if [ ! -f "META-INF/MANIFEST.MF" ]; then
-    echo "[ERROR] The application is missing the META-INF/MANIFEST.MF file."
-    exit 1
+if [[ -z "${SPRING_BOOT_LAUNCHER}" ]]; then
+    if [ ! -f "META-INF/MANIFEST.MF" ]; then
+        echo "[ERROR] The application is missing the META-INF/MANIFEST.MF file."
+        exit 1
+    fi
+    SPRING_BOOT_LAUNCHER=$(grep Main-Class META-INF/MANIFEST.MF | cut -d ' ' -f 2 | tr -d '\r')
 fi
-SPRING_BOOT_MAIN_CLASS=$(grep Main-Class META-INF/MANIFEST.MF | cut -d ' ' -f 2 | tr -d '\r')
-SPRING_BOOT_LAUNCHER=${SPRING_BOOT_LAUNCHER:-${SPRING_BOOT_MAIN_CLASS:-org.springframework.boot.loader.JarLauncher}}
+SPRING_BOOT_LAUNCHER=${SPRING_BOOT_LAUNCHER:-org.springframework.boot.loader.launch.JarLauncher}
 exec java "$@" "$SPRING_BOOT_LAUNCHER"
